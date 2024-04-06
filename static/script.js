b0ffdf61743a7f73c375704a1a4c32ab12bc88ae
JustinX2 <129993011+JustinX2@users.noreply.github.com>
2024-04-06 11:14:11 -0700

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const loginForm=document.getElementById("login-form");
+    if (loginForm){
+        loginForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            console.log("Login form submitted")
+        });
+    }
+
+    const registerForm = document.getElementById("register-form");
+    if (registerForm) {
+        registerForm.addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            console.log("Register form submitted");
+        });
+    }
+
+    const recipeForm = document.getElementById("recipe-form");
+    if (recipeForm) {
+        recipeForm.addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            console.log("Recipe form submitted");
+        });
+    }
+
+    const logoutButton = document.getElementById("logout-button");
+    if (logoutButton) {
+        logoutButton.addEventListener("click", function() {
+            console.log("Logout initiated");
+        });
+    }
+
+})
