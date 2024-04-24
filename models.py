771fcdde4d63d7a958adc4a42b5713f476d216c2
JustinX2 <129993011+JustinX2@users.noreply.github.com>
2024-04-24 15:02:35 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import UserMixin  # Import UserMixin
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-class User(db.Model):
+class User(db.Model, UserMixin):  # Added UserMixin
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
@@ -12,7 +13,7 @@ class User(db.Model):
     recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
 
     def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password)
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
